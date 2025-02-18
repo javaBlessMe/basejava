62a0a2c3da19992a62cec25a0769e6600fc86fd7
javaBlessMe <57236195+javaBlessMe@users.noreply.github.com>
2025-02-18 21:40:18 +0300

    HomeWork_1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,53 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int resumeQuantity = 0;
 
     void clear() {
+        for (int i = 0; i < resumeQuantity; i++) {
+            storage[i] = null;
+        }
+        resumeQuantity = 0;
     }
 
     void save(Resume r) {
+        storage[resumeQuantity] = r;
+        resumeQuantity++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < resumeQuantity; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        Resume[] tempStorage = new Resume[10000];
+        int pointDivergence = 0;
+        for (int i = 0; i < resumeQuantity; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                pointDivergence++;
+                continue;
+            }
+            tempStorage[i - pointDivergence] = storage[i];
+        }
+        storage = tempStorage;
+        resumeQuantity--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] filledStorage = new Resume[resumeQuantity];
+        System.arraycopy(storage, 0, filledStorage, 0, resumeQuantity);
+        return filledStorage;
     }
 
     int size() {
-        return 0;
+        return resumeQuantity;
     }
 }
