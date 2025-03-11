3f21482e3650b1e6d1fcc451985c1a5056e64a5a
javaBlessMe <57236195+javaBlessMe@users.noreply.github.com>
2025-03-11 22:27:19 +0300

    HomeWork_2_update

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,68 +2,67 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private final int STORAGE_LIMIT = 10000;
+    protected final Resume[] storage = new Resume[STORAGE_LIMIT];
     private int resumeCount = 0;
 
     public void clear() {
-        for (int i = 0; i < resumeCount; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, null);
         resumeCount = 0;
     }
 
     public void save(Resume r) {
         if (resumeCount == storage.length) {
             System.out.println("В базу уже записано максимально возможное количество резюме - " + resumeCount);
-        } else if (getPosition(r.uuid) < 0) {
+        } else if (getPosition(r.uuid) >= 0) {
+            System.out.println("Резюме с uiid " + r.uuid + " уже есть в базе");
+        } else {
             storage[resumeCount] = r;
             resumeCount++;
-        } else {
-            System.out.println("Резюме с uiid " + r.uuid + " уже есть в базе");
         }
 
     }
 
     public Resume get(String uuid) {
-        if (getPosition(uuid) >= 0) {
-            return storage[getPosition(uuid)];
-        } else {
+        if (getPosition(uuid) < 0) {
             System.out.println("Резюме с uiid " + uuid + " нет базе");
             return null;
         }
+        return storage[getPosition(uuid)];
     }
 
     public void delete(String uuid) {
         int position = getPosition(uuid);
-        if (position >= 0) {
-            storage[position] = storage[resumeCount - 1];
-            storage[resumeCount - 1] = null;
-            resumeCount--;
-        } else {
+        if (position < 0) {
             System.out.println("Резюме с uiid " + uuid + " не существует");
+            return;
         }
+
+        storage[position] = storage[resumeCount - 1];
+        storage[resumeCount - 1] = null;
+        resumeCount--;
     }
 
     public void update(Resume r) {
         int position = getPosition(r.uuid);
-        if (position > 0) {
-            storage[position] = r;
-        } else {
+        if (position < 0) {
             System.out.println("Резюме с uiid " + r.uuid + " не существует");
+            return;
         }
+        storage[position] = r;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] onlyResumes = new Resume[resumeCount];
-        System.arraycopy(storage, 0, onlyResumes, 0, resumeCount);
-        return onlyResumes;
+        return Arrays.copyOf(storage, resumeCount);
     }
 
     public int size() {
