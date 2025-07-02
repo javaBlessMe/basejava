7c13c9d7ac13b6b3f8846de264240b56618e446f
javaBlessMe <57236195+javaBlessMe@users.noreply.github.com>
2025-07-02 22:39:24 +0300

    HomeWork_6_start

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -3,8 +3,20 @@ package com.urise.webapp.storage;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
+
+    /*
+    private static class ResumeComparator implements Comparator<Resume> {
+        @Override
+        public int compare(Resume o1, Resume o2) {
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
+    }
+*/
+
+    private static final Comparator<Resume> RESUME_COMPARATOR = (o1, o2) -> o1.getUuid().compareTo(o2.getUuid());
 
     @Override
     protected void addResume(int resumeCount, Resume r) {
@@ -17,7 +29,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected Object getSearchKey(String uuid) {
         Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, resumeCount, searchKey);
+        return Arrays.binarySearch(storage, 0, resumeCount, RESUME_COMPARATOR);
     }
 
     @Override
